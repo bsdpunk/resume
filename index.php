87b91c489ee750934605d093b48f2fe030118be5
root <root@localhost.localdomain>
2016-08-17 17:59:39 -0500

    Resume

---
+++ index.php
@@ -0,0 +1,187 @@
+<pre>
+File: *manpages*,  Node: dusty,  Up: (dir)
+
+DUSTY(1)                     General Commands Manual                    DUSTY(1)
+
+
+
+NAME
+       dusty - Dusty Carver, (615) 436-0496, Nashville, TN 
+
+SYNOPSIS
+       dusty -?
+       dusty --help
+       dusty -V
+       dusty --version
+       dusty [-[+]aABcCdeEfFgGiIJKLmMnNqQrRsSuUVwWX~]
+            [-b space] [-h lines] [-j line] [-k keyfile]
+            [-{oO} logfile] [-p pattern] [-P prompt] [-t tag]
+            [-T tagsfile] [-x tab,...] [-y lines] [-[z] lines]
+            [-# shift] [+[+]cmd] [--] [filename]...
+       (See  the  OPTIONS section for alternate option syntax with long option
+       names.)
+
+
+DESCRIPTION
+       dusty is a Unix and Linux systems administrator as well as a python, 
+golang, and node developer. With more than 10 years experience in syst-
+       ems administration, as well as five plus years in development.
+
+WORK HISTORY
+       
+       Linux Administrator                             August 2014 – July 2015 
+       Rackspace Hosting                                       San Antonio, TX
+
+
+       Supported customers Solving Automated alerts, via tickets
+       Took International Customers phone calls And Supported them.
+       Performing Linux maintenance on International Customers Machines 
+       Developed a Python tool specifically for use with the RS API.
+       Responsible for performing Linux maintenance and administration.
+       Installation and configuration of Apache web server, extended base 
+       functionality with third-party modules.
+       Troubleshooting Magento setup and installation and caching 
+       functionality.
+       Custom Python Development to implement business logic for manual 
+       Linux administration tasks.
+       Used OpenStack extensively to manage cloud servers.
+            
+
+ 
+       Systems Administrator/Web Developer                                                                          
+       September 2013 – August 2014  Freelance 
+                                                                                                                                                          
+       Nashville, TN  Worked with a variety of clients in the telecom industry, 
+       upporting web development efforts to implement evolving business requirements.
+       Developed a telecommunications portal for buying and selling cell phones.  
+       reated multiple Wordpress and informational sites. As well as other e-commerce 
+       ites.
+       Used Django Web Development framework to create databases backed 
+       pplications, utilized MySQL.
+       PHP Development with Propel, an operational/relational database 
+       anagement tool.
+       Developed database driven Node.js application for online surveys, similar 
+       o a tool like Survey Monkey.
+       Used jQuery to create interactive and response web user interface for web 
+       pplications.
+       Developed complex backend Perl scripts, utilized ModPerl and CGI.
+       
+       Developer/Unix Admin                 
+       March 2013 – September 2013  Horton Group
+       Nashville, TN
+       Drupal Development and Unix Administration
+       Ubuntu Linux servers
+       PHP Development with Drupal
+       jQuery Development
+       Cloud Experience
+       Linux Administration
+       Mac Server and Desktop 
+       
+       
+       Developer And Unix Admin                                                                                           
+       April 2012 – July 2012 (contract)
+       Ten Fast Feet
+
+       Nashville, TN
+       My original contract was for moving sites over, from two different 
+       Environments, but quickly expanded to PHP(propel) development
+       Various Linux Maintenance
+       PHP Development with Propel
+       jQuery Development
+       Mac Server and Desktop
+       
+       Developer                                                                                                                
+       January 2012 – October 2012 (contract)
+       Capital Accounts Maintaining a debt 
+       Collection database and it’s interface for the call center employees
+       RoR Development with MSSQL
+       PHP Development PDO and MSSQL and MySQL
+       Python Development
+       Javascript interface components    
+       
+       
+       Developer                                                                                                                
+       October 2010 – October 2011 (contract) Mars 
+       Franklin, TN
+       Maintaining and creating new features for FOSwiki
+       Perl Development and MySQL
+       Maintenance and New Feature creation of several FOSwiki wikis
+       Maintained on a windows server
+       Javascript Features including ajax and jQuery   
+       
+       Developer                                                                                                       
+       September 2010 - November 2010 (contract) Music and Books 
+       hristian                                                                                                                   
+       ranklin, Tennessee Worked on slicehost Linux servers(Ubuntu)
+       Development of Internal tool involving both the integration of the 
+       olusion API and a 3PL(Third Party Logistics) Application
+       PHP Development and MySQL Code Igniter
+       Maintenance of Musichristian.com
+       Parsing an XML file, and dumping select contents into a MySQL database
+       Reading and writing of proprietary format files(CDF-lite), from Ingram 
+       ooks
+       Administration of the MySQL database 
+       
+       Developer/Linux Systems Administrator                                                                          
+       November 2008 -August 2010
+       Dobie Media  Worked on amazon cloud servers, creating and maintaining 
+       Heavy Cloud Experience, Experience with High Availability and Scaling
+       Development of Perl Sys Admin tools
+       Development on the back end of the main Eviesays.com app (Mod_Perl)
+       Scraping off other websites for insertion into MySQL Database using Perl 
+       cripts
+       Setting up and maintaining the apache servers for a highly scalable 
+       pplication (Eviesays)
+       Eviesays used MySQL and the Perl DBI, to access the database
+       Wore a lot of hats, this is a small startup  
+       
+       Web Developer                                                                                                                
+       September 2008 - November 2008 
+       Freelance
+       Nashville, Tennessee  PHP and Perl development of internal web applications
+       Development of Perl And Python clients
+       SEO experience 
+       
+       Network Consultant                                                                                                        
+       ecember 2007 - September 2008 Dynamic Edge                                                                     
+       ashville, TN/Ann Arbor, MI Wide Variety of networking and OS experience, with a 
+       ocus on 2003 SBS, and Debian Linux
+       Perl and PHP web app development  SEO experience
+       As a consultant I interacted with the customer in such a way that I made 
+       ecommendations and explained complex issues
+       Cisco WAN Experience
+       Both Postgres and MySQL servers, were used for DE projects  
+       
+       IT Assistant                                                                             
+       November 2006 -October 2007 cj 
+       Advertising                                                                                                              
+       Nashville, TN FreeBSD Experience (Apache)
+       Hybrid(VOIP/POTS) Altigen Phone experience
+       Troubleshooting and PC repair
+       Exchange 2007 Experience in a Windows 2003 Active Directory Domain
+       Cisco Experience 
+       
+       Network Administrator                                                           
+       February 2006-November 2006 iTom                                                                                                        
+       ashville, TN
+       Primary information security officer
+       Windows Server 2003\Exchange 2003 experience
+       Built asterisk servers, a Linux VoIP Solution
+       Troubleshooting and PC Repair  
+
+EDUCATION
+
+       High-Tech Institute                                                                                                      
+       July 2006 Associates of Applied Science                                                                                  
+       Nashville, TN
+       Computer Networking and Security Major
+       
+       
+CERTIFICATIONS AND AWARDS
+
+       A+ Certified
+       Network+ Certified
+       Microsoft Certified Professional
+       High-Tech Institute Information Technology Award Recipient
+       Silver Medal in the National Latin Exam
+       </pre>
